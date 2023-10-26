ce7a53fc4874817d794ad5b2d1e3b512bfd76963
Isuru Pathirathna <isuru2002@gmail.com>
2023-10-26 11:27:14 +0530

    featured products

--- client/src/app/components/categories/categories.component.ts
+++ client/src/app/components/categories/categories.component.ts
@@ -18,19 +18,19 @@ export class CategoriesComponent {
     },
     {
       name: 'Arts',
-      description: 'Dive into the world of creativity and self-expression with our diverse collection of arts.',
+      description: 'Dive into the world of creativity and self-expression with our diverse collection.',
       imageSrc: 'https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2494.jpg?w=1380&t=st=1697902967~exp=1697903567~hmac=1bb20b863ef8693a784247eb3886a245da814cf394b312a5ecf63ff404ea6638',
       link: '/category/arts',
     },
     {
       name: 'UX',
-      description: 'Enhance user experiences and design with cutting-edge user interface and user experience resources.',
+      description: 'Enhance user experiences and design with cutting-edge user interface and user.',
       imageSrc: 'https://img.freepik.com/free-vector/gradient-ui-ux-elements-background_23-2149056159.jpg?t=st=1697903176~exp=1697903776~hmac=c957b08d3daeadb724d29fa245f0c92dbd1f5abd0d781268701c1f6d7d119994',
       link: '/category/ux',
     },
     {
       name: 'Philosophy',
-      description: 'Delve into the depths of philosophical thought and contemplation with our philosophical resources.',
+      description: 'Delve into the depths of philosophical thought and contemplation with our.',
       imageSrc: 'https://img.freepik.com/free-vector/hand-drawn-mindfulness-concept-with-characters_52683-69073.jpg?w=740&t=st=1697903055~exp=1697903655~hmac=67f080c9c8752ff32fa363864b231c572de07466125b74f6bae9b52320e3ab56',
       link: '/category/Philosophy',
     },
